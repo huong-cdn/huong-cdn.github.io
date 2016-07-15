ab108164cb196f9eddc78de26f6d346402abcceb
huongnx <xuan.huong.humg@gmail.com>
2016-07-15 10:08:47 +0700

    update assets for vacxin_gate

--- vacxin_gate/js/fix.js
+++ vacxin_gate/js/fix.js
@@ -1,3 +1,12 @@
 /**
  * Created by huongnx on 24/06/2016.
  */
+jQuery(document).ready(function($) {
+	 $('#comingSoon').on('show.bs.modal', function(){
+	    var myModal = $(this);
+	    clearTimeout(myModal.data('hideInterval'));
+	    myModal.data('hideInterval', setTimeout(function(){
+	        myModal.modal('hide');
+	    }, 1500));
+	});
+});
